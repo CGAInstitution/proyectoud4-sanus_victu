cfceaababf07a6ba10cb1e52b4db72313e7912d1
bestiadivina147 <diegopc147@gmail.com>
2025-02-16 19:40:30 +0100

    feat: :sparkles: add nutricionista.html

--- src/main/java/madstodolist/controller/NutricionistaController.java
+++ src/main/java/madstodolist/controller/NutricionistaController.java
@@ -1,4 +1,38 @@
 package madstodolist.controller;
 
+import madstodolist.model.Nutricionista;
+import madstodolist.model.Usuario;
+import madstodolist.repository.nutricionistaRepository;
+import madstodolist.repository.usuarioRepository;
+import madstodolist.repository.usuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Set;
+
+@Controller
+
 public class NutricionistaController {
+
+    @Autowired
+    private nutricionistaRepository nutricionRepository;
+
+    @Autowired
+    private usuarioRepository userRepository;
+
+    @GetMapping("/nutricionista/{id}/usuarios")
+    public String verUsuarios(@PathVariable("id") long id, Model model) {
+        // Obtenemos el nutricionista por su ID
+        Nutricionista nutricionista = nutricionRepository.findById(id).orElse(null);
+
+        // Si el nutricionista existe, mostramos sus pacientes (usuarios)
+        if (nutricionista != null) {
+            Set<Usuario> pacientes = nutricionista.getPacientes();
+            model.addAttribute("pacientes", pacientes);
+        }
+        return "nutricionista";
+    }
 }
