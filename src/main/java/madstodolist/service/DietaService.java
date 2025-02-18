a1d951b5f009bae3a0c5bbe38c404b97b7d8e3c7
bestiadivina147 <diegopc147@gmail.com>
2025-02-18 09:39:05 +0100

    :feat:sparkles: changes in formUsuario.html

--- src/main/java/madstodolist/service/DietaService.java
+++ src/main/java/madstodolist/service/DietaService.java
@@ -1,10 +1,17 @@
 package madstodolist.service;
 
+import madstodolist.repository.dietaRepository;
+import madstodolist.model.Dieta;  // Asegúrate de tener la clase Dieta importada
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DietaService {
 
+    @Autowired
+    private dietaRepository dietaRepository;
 
-
+    public Dieta guardarDieta(Dieta dieta) {
+        return dietaRepository.save(dieta);
+    }
 }
